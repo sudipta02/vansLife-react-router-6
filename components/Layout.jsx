1c39f1aae75152cfd72621b8206158358030c987
sudipta02 <sudipta.sundar95@gmail.com>
2023-06-30 12:08:29 +0530

    Changes

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout() {
     <>
       <Header />
       <Outlet />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function HostVans() {
-  return <h1>Host Vans list page</h1>;
+  const [hostVans, setHostVans] = useState([]);
+  useEffect(() => {
+    const dataFetch = async () => {
+      await fetch("/api/host/vans")
+        .then((res) => res.json())
+        .then((result) => setHostVans(result.vans));
+    };
+    dataFetch();
+  }, []);
+
+  const HostVan = ({ name, price, imageUrl }) => {
+    return (
+      <div className="host-van-item-container">
+        <img src={imageUrl} />
+        <div>
+          <p>{name}</p>
+          <p>₹{price}/day</p>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <>
+      <h1>Your listed vans</h1>
+      <div className="host-vans-list-container">
+        {hostVans.map(({ name, price, imageUrl }) => (
+          <HostVan price={price} name={name} imageUrl={imageUrl} />
+        ))}
+      </div>
+    </>
+  );
 }
